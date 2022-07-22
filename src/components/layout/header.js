1f73dab6bd0bdcf4f8b40b50e12d4c5af6632ce2
Pasindu Dinal Sumanasekara <51123579+mrmihi@users.noreply.github.com>
2022-07-22 17:10:26 +0530

    Added a tilt effect to the theme toggler

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -11,9 +11,9 @@ const Header = props => {
     <header>
         <div onClick={() => setTheme(colorTheme)} className="h-10 w-10 xl:h-12 xl:w-12 absolute top-12 right-12 rounded-full ">
             { colorTheme == 'light' ? (
-            <BsSunFill className="cursor-pointer h-10 w-10 xl:h-12 xl:w-12  fill-white"/> ):
+            <BsSunFill className="cursor-pointer h-10 w-10 xl:h-12 xl:w-12  fill-white hover:rotate-12 transition-all"/> ):
             (
-            <BsMoonStarsFill className="cursor-pointer h-10 w-10 xl:h-12 xl:w-12 "/> )
+            <BsMoonStarsFill className="cursor-pointer h-10 w-10 xl:h-12 xl:w-12 hover:rotate-12 transition-all"/> )
             }
         </div>
     </header>
