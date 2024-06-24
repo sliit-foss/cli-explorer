0867b6afad62632ad0d614e6c6f7eef4e2a4c9d7
icy-r <asath12882@gmail.com>
2024-06-24 19:28:08 +0530

    Added Vue commands #7

---
+++ src/data/vue.js/primary-options.js
@@ -0,0 +1,22 @@
+const options = [
+  { value: "create", label: "create" },
+  { value: "ui", label: "ui" },
+  { value: "serve", label: "serve" },
+  { value: "build", label: "build" },
+  { value: "lint", label: "lint" },
+  { value: "add", label: "add" },
+  { value: "invoke", label: "invoke" },
+  { value: "inspect", label: "inspect" },
+  { value: "install", label: "install" },
+  { value: "upgrade", label: "upgrade" },
+];
+
+export const primaryOptions = options.sort((x, y) => {
+  if (x.value < y.value) {
+    return -1;
+  }
+  if (x.value > y.value) {
+    return 1;
+  }
+  return 0;
+});
