9ec3945b122604af6d95b813543fcb227f54628b
icy-r <asath12882@gmail.com>
2024-08-03 21:19:57 +0530

    chore: Refactor Vue.js imports
    The Vue.js imports in the  file were refactored. Additionally, the  file was deleted as it is no longer needed.

--- src/data/vue.js/index.js
+++ src/data/vue.js/index.js
@@ -1,3 +1,2 @@
-export { primaryOptions as optionsFirst } from './primary-options';
-export { secondaryOptions as optionsSecond } from './secondary-options';
-export { tertiaryOptions as optionsThird } from './tertiary-options';
+export { primaryOptions as optionsFirst } from "./primary-options";
+export { secondaryOptions as optionsSecond } from "./secondary-options";
